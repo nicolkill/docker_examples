564a286961140fa9f57df0cd3dbd20046fc2ef74
Nicol Acosta <nicolacosta8@gmail.com>
2018-11-28 11:59:50 -0600

    Overriden!!

--- app/app.js
+++ app/app.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 const host = '0.0.0.0';
 const app = express();
 
+console.log(process.env.ANOTHER_ENV_VAR);
+
 app.get('/', (req, res) => res.json(data.func()));
 
 app.listen(port, host, () => console.log(`Example app listening on port http://localhost:${port}!`))
